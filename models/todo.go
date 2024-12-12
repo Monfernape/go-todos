c1db57d17170ae1682d9c0620703aed0f170a6ee
Monfernape <engr.usmankhalil@gmail.com>
2024-12-12 20:23:41 +0500

    feat: added update and delete functionality

--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,10 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Todo struct {
-	Id        string `json:"_id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
+	Id        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title     string             `json:"title"`
+	Completed bool               `json:"completed"`
+	IsDeleted bool               `json:"isDeleted"`
 }
